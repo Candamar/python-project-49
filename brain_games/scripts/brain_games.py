bc8d822fe5520a757b41eb51467e2b5229097ede
Alexey Khvostantsev <ax2303@gmail.com>
2022-11-22 00:19:30 +0500

    fix .gitignore

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,13 @@
 #! /usr/bin/env python
 
+
 from ..cli import ask_name
+
 
 def main():
     print('Welcome to the Brain Games!')
     ask_name()
 
+
 if __name__ == '__main__':
-   main()
+    main()
