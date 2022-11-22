b3cd94293c3c7386c6db0937a393f884cef69483
Alexey Khvostantsev <ax2303@gmail.com>
2022-11-22 22:32:57 +0500

    add game_engine.py

---
+++ brain_games/game_engine.py
@@ -0,0 +1,27 @@
+import prompt
+
+
+def game(rules, qa_pairs):
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    print(rules)
+    for (question, answer) in qa_pairs.items():
+        print(f'Question: {question}')
+        input_answer = prompt.string('Your answer: ')
+        if input_answer == answer:
+            print('Correct!')
+        else:
+            print(f'"{input_answer}" is wrong answer ;(. '
+                  f'Correct answer was "{answer}".')
+            print(f"Let's try again, {name}!")
+            return False
+    print(f'Congragulations, {name}!')
+    return True
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
