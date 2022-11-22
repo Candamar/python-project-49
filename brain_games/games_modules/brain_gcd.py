4f177617fced5505ffcb3ead28659421a1325b40
Alexey Khvostantsev <ax2303@gmail.com>
2022-11-23 00:09:32 +0500

    add brain_gcd

---
+++ brain_games/games_modules/brain_gcd.py
@@ -0,0 +1,28 @@
+from random import randrange
+from ..game_engine import game
+
+
+def gcd(num1, num2):
+    while num1 != num2:
+        if num1 > num2:
+            num1 = num1 - num2
+        else:
+            num2 = num2 - num1        
+    return num1
+
+
+def main():
+    max_num = 100
+    rules = 'Find the greatest common divisor of given numbers.'
+    question_answer_pairs = {}
+    for _ in range(3):
+        num1 = randrange(max_num)
+        num2 = randrange(max_num)
+        question = f'{str(num1)} {str(num2)}'
+        answer = gcd(num1, num2)
+        question_answer_pairs[question] = str(answer)
+    game(rules, question_answer_pairs)
+
+
+if __name__ == '__main__':
+    main()
---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+
+
+from ..games_modules.brain_gcd import main as brain_gcd
+
+
+def main():
+    brain_gcd()
+
+
+if __name__ == '__main__':
+    main()
