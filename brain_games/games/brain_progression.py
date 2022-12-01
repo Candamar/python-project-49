268a3987955edd63db27b2f8116d549f236b698a
Alexey Khvostantsev <ax2303@gmail.com>
2022-12-01 19:45:09 +0500

    brain_progression.py

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -1,26 +1,20 @@
 from random import randrange
-from ..game_engine import game
 
 
-def main():
+def rules():
+    print('What number is missing in the progression?')
+
+
+def get_qa_pair():
     PROGRESSION_MAX_START = 100
     MAX_STEP = 10
-    rules = 'What number is missing in the progression?'
-    question_answer_pairs = {}
-    for _ in range(3):
-        num = randrange(PROGRESSION_MAX_START)
-        step = randrange(MAX_STEP)
-        progression = []
-        for _ in range(10):
-            progression.append(str(num))
-            num += step
-        index = randrange(10)
-        answer = progression[index]
-        progression[index] = '..'
-        question = ' '.join(progression)
-        question_answer_pairs[question] = answer
-    game(rules, question_answer_pairs)
-
-
-if __name__ == '__main__':
-    main()
+    num = randrange(PROGRESSION_MAX_START)
+    step = randrange(MAX_STEP)
+    progression = []
+    for _ in range(10):
+        progression.append(str(num))
+        num += step
+    index = randrange(10)
+    answer = progression[index]
+    progression[index] = '..'
+    return ' '.join(progression), answer
