9edf33f84b120b4bd9de90ced0e95239ba6cfaa0
Alexey Khvostantsev <ax2303@gmail.com>
2022-12-01 19:17:08 +0500

    fix game_calc.py

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -1,24 +1,19 @@
 from random import randrange
 from random import choice
-from ..game_engine import game
 
 
-def main():
+def rules():
+    print('What is the result of the expression?')
+
+
+def get_qa_pair():
     MAX_NUM = 100
-    rules = 'What is the result of the expression?'
-    operators = ('+', '-', '*')
-    question_answer_pairs = {}
-    for _ in range(3):
-        num1 = randrange(MAX_NUM)
-        num2 = randrange(MAX_NUM)
-        operator = choice(operators)
-        question = f'{str(num1)} {operator} {str(num2)}'
-        answer = (num1 + num2 if operator == '+' else
-                  num1 - num2 if operator == '-' else
-                  num1 * num2)
-        question_answer_pairs[question] = str(answer)
-    game(rules, question_answer_pairs)
-
-
-if __name__ == '__main__':
-    main()
+    operators = ("+", "-", "*")
+    num1 = randrange(MAX_NUM)
+    num2 = randrange(MAX_NUM)
+    operator = choice(operators)
+    question = f'{str(num1)} {operator} {str(num2)}'
+    answer = (num1 + num2 if operator == '+' else
+              num1 - num2 if operator == '-' else
+              num1 * num2)
+    return question, str(answer)
